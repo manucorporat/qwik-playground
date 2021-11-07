e9bef9f122e39e82983a92d5a7b0d55b771e4afb
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2021-11-07 05:21:59 +0000

    🍅

---
+++ public/wasm-web/qwik_wasm_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function transform_modules(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
